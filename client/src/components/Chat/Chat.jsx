16afd73fef85d2f28f7117737a74d7a6648e8352
Amit Verma <amitverma@jpfunware.com>
2024-11-01 11:12:50 +0530

    some changes

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,150 +1,102 @@
 import React, { useState, useEffect } from "react";
 import Sidebar from "../Sidebar/Sidebar";
-import { socketInit, sendDirectMessage } from "../../services/websocket";
-import {uploadFile} from "../../services/api.js"
+import { socketInit, sendDirectMessage } from "../../services/websocket.js";
+import { uploadFile } from "../../services/api.js";
 import styles from "./Chat.module.css";
-import SeachPopUp from "../SearchPopUp/SeachPopUp"
-import CreateGroupPopup from "../CreateGroupPopup/CreateGroupPopup";
+import SearchPopUp from "../SearchPopUp/SeachPopUp"; // Corrected import
+import CreateGroupPopup from "../CreateGroupPopup/CreateGroupPopup.jsx";
+
 function Chat({ loginData }) {
   const [message, setMessage] = useState("");
-  const [isCreateGrpPopupOpen,setIsCreateGrpPopupOpen]=useState(false);
-  const[disableButton,setDisableButton] = useState(false)
-  
-  
-  // const [messages, setMessages] = useState([{username:"Atul", message:"Hey, How are you?"},
-  //     {username:"Atul", message:"Hey, How are you doing?"},
-  //     {username:"Atul", message:"Hey, How are you?"},{username:"Atul", message:"Hey, How are you doing?"},
-  //     {username:"Atul", message:"Hey, How are you?"},{username:"Atul", message:"Hey, How are you doing?"}]);
-  //   const [messages, setMessages] = useState({
-  //     direct: {
-  //       "amit@gmail.com": [
-  //         { email: "av416044@gmail.com", message: "hello", name: "amit" },
-  //         { email: "abc@gmail.com", message: "yes", name: "test" },
-  //       ],
-  //     },
-  //     room: {
-  //       room1: [
-  //         { email: "amit@gmail.com", message: "hello", name: "amit" },
-  //         { email: "av416044@gmail.com", message: "yes room", name: "test" },
-  //       ],
-  //     },
-  //   });
-  const [messages, setMessages] = useState({
-    direct: {},
-    room: {},
-  });
-
-  useEffect(()=>{
-    if(isCreateGrpPopupOpen)
-      setIsCreateGrpPopupOpen(true);
-  },[isCreateGrpPopupOpen]);
+  const [isCreateGrpPopupOpen, setIsCreateGrpPopupOpen] = useState(false);
+  const [disableButton, setDisableButton] = useState(false);
+  const [messages, setMessages] = useState({ direct: {}, room: {} });
   const [selectedUser, setSelectedUser] = useState(null);
-//   const [usersAndRoom, setUsersAndRoom] = useState([
-//     {
-//       email: "amit@gmail.com",
-//       name: "Amit",
-//     },
-//     {
-//       roomName: "room1",
-//     },
-//   ]);
   const [usersAndRoom, setUsersAndRoom] = useState([]);
-  useEffect(()=>{
-    console.log("vsal")
-    console.log(usersAndRoom)
-  },[usersAndRoom])
-  const[searchResult,setSearchResult]= useState(null);
+  const [searchResult, setSearchResult] = useState(null);
 
   useEffect(() => {
     const socket = socketInit(loginData?.accessToken);
     if (socket) {
-      //console.log("====socket connected");
       socket.on("searchUser", (data) => {
-       console.log("=====search user data====", data);
-        setSearchResult(data)
-
-
+        console.log("=====search user data====", data);
+        setSearchResult(data);
       });
-      //on direct message
+
       socket.on("directMessage", (data) => {
         console.log("=====direct message data====", data);
-        // handle incoming direct message
-        let msg = messages["direct"][data.from]
-        if(!msg){
-            msg = [{
-                email:data.from,
-                message:data.message
-            }]
-        }else{
-            msg.push({
-                email:data.from,
-                message:data.message
-            })
-            if(msg.length>100){
-                //remove older message
-                msg.shift()
-            }
+        let msg = messages.direct[data.from] || [];
+        console.log(messages)
+        msg.push({
+          email: data.from,
+          message: data.message,
+          name: data.name,
+        });
+
+        if (msg.length > 100) {
+          msg.shift();
         }
-        if(!usersAndRoom.includes(data.form)){
-            setUsersAndRoom([...usersAndRoom,{email:data.from,name:data.name}])
+        console.log()
+        setMessages((prevMessages) => ({
+          ...prevMessages,
+          direct: {
+            ...prevMessages.direct,
+            [data.from]: msg,
+          },
+        }));
+        const users=[...usersAndRoom]
+         const findUser = users.find(val=>val.email == data.from)
+         console.log("======>",findUser)
+        if (!findUser) {
+          console.log(usersAndRoom)
+          setUsersAndRoom((prev) => [...prev, { email: data.from, name: data.name }]);
         }
-        let finalMessage  = {...messages}
-        finalMessage["direct"][data.from] = msg
-        console.log(finalMessage)
-        setMessages(finalMessage)
       });
 
-      //on disconnect
-      socket.on("userDisconnected",(data)=>{
+      socket.on("userDisconnected",(data) => {
         console.log("=====user disconnected====", data);
-      })
+        const users = [...usersAndRoom]
+        console.log(users)
+        const filterUsers = users.filter(val=> val.email!=data.email)
+        setUsersAndRoom(filterUsers)
+      });
     }
-  }, []);
+  }, [loginData?.accessToken]);
 
   const handleSendMessage = () => {
-    if(selectedUser?.email){
-        let msg = messages["direct"][selectedUser.email]
-        if(!msg){
-            msg = [{
-                email:loginData?.email,
-                message:message,
-            }]
-        }else{
-            msg.push({
-                email:loginData?.email,
-                message:message,
-            })
-            if(msg.length>100){
-                //remove older message
-                msg.shift()
-            }
-        }
-        let finalMessage  = {...messages}
-        finalMessage["direct"][selectedUser.email] = msg
-        console.log(finalMessage)
-        setMessages(finalMessage)
-        //direct chat
-        sendDirectMessage({
-            message: message,
-            to:selectedUser?.email
-        })
-    }else if(selectedUser?.roomName){
-        //group chat
+    if (selectedUser?.email) {
+      let msg = messages.direct[selectedUser.email] || [];
+      msg.push({
+        email: loginData.email,
+        message: message,
+        name: loginData.name || loginData.email,
+      });
 
+      if (msg.length > 100) {
+        msg.shift();
+      }
+
+      setMessages((prevMessages) => ({
+        ...prevMessages,
+        direct: {
+          ...prevMessages.direct,
+          [selectedUser.email]: msg,
+        },
+      }));
+
+      sendDirectMessage({
+        message: message,
+        to: selectedUser.email,
+      });
     }
     setMessage("");
   };
 
-  const onUserAndRoomSelect = (data, room = false) => {
+  const onUserAndRoomSelect = (data) => {
     setSelectedUser(data);
   };
-  const handleSearchUser = (data)=>{      
-    const emailExists = usersAndRoom.some(item => item.email === data.email);    
-    if(!emailExists)
-      setUsersAndRoom([...usersAndRoom,{...data}])
-  }
 
-  const handleFileUpload = async(file)=>{
+  const handleFileUpload = async (file) => {
     if (!file) {
       alert('Please select a file first!');
       return;
@@ -153,39 +105,43 @@ function Chat({ loginData }) {
     formData.append('media', file);
 
     try {
-      const response = await uploadFile(formData,loginData.accessToken)
-      if(response){
-        setMessage(response)
+      const response = await uploadFile(formData, loginData.accessToken);
+      if (response) {
+        setMessage(response);
       }
-      
-     } catch (error) {
+    } catch (error) {
       console.error('Error uploading file:', error);
-    }finally{
-      setDisableButton(false)
+    } finally {
+      setDisableButton(false);
     }
-  }
+  };
 
   const handleFileChange = (event) => {
-    const file = event.target.files[0]
-    if (!file) 
-      return;
+    const file = event.target.files[0];
+    if (!file) return;
+
     const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2 MB in bytes
     if (file.size > MAX_FILE_SIZE) {
-        alert('File size exceeds 2 MB. Please select a smaller file.');
-        return;
+      alert('File size exceeds 2 MB. Please select a smaller file.');
+      return;
     }
-    setDisableButton(true)
-    handleFileUpload(file)
+
+    setDisableButton(true);
+    handleFileUpload(file);
+  };
+
+  // Define the handleSearchUser function
+  const handleSearchUser = (data) => {
+    const emailExists = usersAndRoom.some(item => item.email === data.email);
+    if (!emailExists) {
+      setUsersAndRoom([...usersAndRoom, { ...data }]);
+    }
   };
 
   return (
     <div className={styles.container}>
-        {
-            searchResult && <SeachPopUp searchResult={searchResult} handleSearchUser={handleSearchUser}/>
-        }
-        {
-          isCreateGrpPopupOpen&&<CreateGroupPopup setIsCreateGrpPopupOpen={setIsCreateGrpPopupOpen}/>
-        }
+      {searchResult && <SearchPopUp searchResult={searchResult} handleSearchUser={handleSearchUser} />}
+      {isCreateGrpPopupOpen && <CreateGroupPopup setIsCreateGrpPopupOpen={setIsCreateGrpPopupOpen} />}
       <Sidebar
         usersAndRoom={usersAndRoom}
         onUserAndRoomSelect={onUserAndRoomSelect}
@@ -195,27 +151,15 @@ function Chat({ loginData }) {
       />
       <div className={styles.main}>
         <div className={styles.messages}>
-          {selectedUser &&
-            (selectedUser.email || selectedUser.roomName) &&
-            messages[selectedUser.email ? "direct" : "room"][
-              selectedUser.email ?? selectedUser.roomName
-            ]?.map((msg, index) => (
-              <p
-                key={index}
-                className={
-                  loginData.email != msg.email
-                    ? styles.ownMessage
-                    : styles.message
-                }
-              >
-                <strong>{msg.name}:</strong> {msg.message}
-              </p>
-            ))}
+          {selectedUser && messages.direct[selectedUser.email]?.map((msg, index) => (
+            <p key={index} className={loginData.email !== msg.email ? styles.ownMessage : styles.message}>
+              <strong>{msg.name}:</strong> {msg.message}
+            </p>
+          ))}
         </div>
 
         <div className={styles.msgInput}>
           <input type="file" onChange={handleFileChange} />
-
           <input
             type="text"
             value={message}
@@ -223,7 +167,7 @@ function Chat({ loginData }) {
             placeholder="Type a message..."
             className={styles.input}
           />
-          <button  disabled={disableButton} style={{ opacity: disableButton ? 0.5 : 1 }} className={styles.sendBtn} onClick={handleSendMessage}>
+          <button disabled={disableButton} style={{ opacity: disableButton ? 0.5 : 1 }} className={styles.sendBtn} onClick={handleSendMessage}>
             Send
           </button>
         </div>
